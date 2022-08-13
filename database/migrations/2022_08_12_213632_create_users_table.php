77e71413f3d0eee56ffeae2da286e9ccfa6ab3af
ferouck <darkkfoxx.z@gmail.com>
2022-08-12 21:44:14 -0300

    [feature] Create migration for users

---
+++ database/migrations/2022_08_12_213632_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('email', 55);
+            $table->integer('cpf_cnpj');
+            $table->string('password');
+            $table->enum('type', ['client', 'shop']);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->unique('email');
+            $table->unique('cpf_cnpj');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
