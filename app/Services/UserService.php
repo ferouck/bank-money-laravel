283dd318d7f85666c072ccbaa0f84ea7a5aa9306
ferouck <darkkfoxx.z@gmail.com>
2022-08-13 12:34:51 -0300

    [feature] Create user service

---
+++ app/Services/UserService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use App\Interfaces\UserRepositoryInterface;
+
+class UserService
+{
+    private UserRepositoryInterface $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function create($data)
+    {
+        $data['password'] = bcrypt($data['password']);
+        return $this->userRepository->createUser($data);
+    }
+}
