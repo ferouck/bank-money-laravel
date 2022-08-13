b4982442e1284f9f1c2a5012e2e48816b0dcd700
ferouck <darkkfoxx.z@gmail.com>
2022-08-12 21:44:39 -0300

    [feature] Create table for transfers users

---
+++ database/migrations/2022_08_13_001559_create_transfers_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transfers_users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('payer');
+            $table->foreignId('payee');
+            $table->float('value', 10, 2);            
+            $table->enum('status', ['pending', 'fulfilled']);
+            $table->uuid('transfer_protocol');
+
+            $table->foreign('payer')->references('id')->on('users');
+            $table->foreign('payee')->references('id')->on('users');
+
+            $table->index('transfer_protocol');
+            $table->unique('transfer_protocol');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transfers_users');
+    }
+};
