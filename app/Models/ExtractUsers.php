cf6f50e86951022b6a0def2777c9044b9834bcbd
ferouck <darkkfoxx.z@gmail.com>
2022-08-13 10:12:22 -0300

    [fix] Change plural name for singular in models

--- app/Models/ExtractUsers.php
+++ app/Models/ExtractUsers.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ExtractUsers extends Model
+class ExtractUser extends Model
 {
     use HasFactory;
 
--- app/Models/TransfersUsers.php
+++ app/Models/TransfersUsers.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class TransfersUsers extends Model
+class TransferUser extends Model
 {
     use HasFactory;
 
