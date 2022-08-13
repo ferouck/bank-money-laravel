8a5daccfd7a24e635553ce4069e16595a63b8d83
ferouck <darkkfoxx.z@gmail.com>
2022-08-13 12:34:37 -0300

    [fix] Change user model for recive jwt and manipule correct values

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,25 +2,43 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
+    use HasFactory, Notifiable, SoftDeletes;
 
     protected $fillable = [
         'name',
         'email',
         'password',
+        'cpf_cnpj',
+        'type'
     ];
 
     protected $hidden = [
+        'cpf_cnpj',
         'password',
         'type'
     ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [
+            'user' => [
+                'id' => $this->id,
+                'type' => $this->type
+            ]
+        ];
+    }
 }
