5a0031bf19e02c232cf1ed36fcf167db65fe1cba
ferouck <darkkfoxx.z@gmail.com>
2022-08-14 09:52:49 -0300

    [fix] Add generate uuid in transfer users

--- database/migrations/2022_08_13_001559_create_transfers_users_table.php
+++ database/migrations/2022_08_13_001559_create_transfers_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -19,7 +20,7 @@ return new class extends Migration
             $table->foreignId('payee');
             $table->float('value', 10, 2);
             $table->enum('status', ['pending', 'fulfilled'])->default('pending');
-            $table->uuid('transfer_protocol');
+            $table->uuid('transfer_protocol')->default(DB::raw('(UUID())'));
 
             $table->foreign('payer')->references('id')->on('users');
             $table->foreign('payee')->references('id')->on('users');
