7b1ff1d8a4ab227c0c196467a23a170f361bf61f
ferouck <darkkfoxx.z@gmail.com>
2022-08-14 09:55:54 -0300

    [feature] Create transfer user respository

---
+++ app/Repositories/TransferUserRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\TransferUserRepositoryInterface;
+use App\Models\TransferUser;
+
+class TransferUserRepository implements TransferUserRepositoryInterface
+{
+    public function getTransferByUuid($uuId)
+    {
+        return TransferUser::findOrFail($uuId);
+    }
+
+    public function getAllTransferByUserId($userId)
+    {
+        return TransferUser::where('user_id', $userId)
+                ->orderBy('created_at')
+                ->get();
+    }
+
+    public function deleteTransfer($uuId)
+    {
+        TransferUser::destroy($uuId);
+    }
+
+    public function createTransfer(array $transferData)
+    {
+        return TransferUser::create($transferData);
+    }
+
+    public function updateTransfer($uuId, array $transferData)
+    {
+        return TransferUser::whereId($uuId)->update($transferData);
+    }
+}
