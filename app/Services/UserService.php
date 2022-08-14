19179c215b169b196e22d3f2047d9ca6fbdd0e8f
ferouck <darkkfoxx.z@gmail.com>
2022-08-14 09:53:43 -0300

    [feature] Create method for get info by token

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -3,19 +3,29 @@
 namespace App\Services;
 
 use App\Interfaces\UserRepositoryInterface as Repository;
+use App\Services\AuthService;
 
 class UserService
 {
     private Repository $userRepository;
 
-    public function __construct(Repository $userRepository)
+    private AuthService $authService;
+
+    public function __construct(Repository $userRepository, AuthService $authService)
     {
         $this->userRepository = $userRepository;
+        $this->authService = $authService;
     }
 
     public function create($data)
     {
         $data['password'] = bcrypt($data['password']);
         return $this->userRepository->createUser($data);
+    }
+
+    public function getInfoUserByToken()
+    {
+        $data = $this->authService->getTokenPayload();
+        return $data['user'];
     }
 }
