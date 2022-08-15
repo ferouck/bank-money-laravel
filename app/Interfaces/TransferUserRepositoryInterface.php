fb02388c1269ca2e917910206c57410d19b6bccf
ferouck <darkkfoxx.z@gmail.com>
2022-08-15 18:04:49 -0300

    [feature] Add method for get protocol transfer and update by protocol

--- app/Interfaces/TransferUserRepositoryInterface.php
+++ app/Interfaces/TransferUserRepositoryInterface.php
@@ -6,7 +6,8 @@ interface TransferUserRepositoryInterface
 {
     public function getTransferByUuid($uuId);
     public function getAllTransferByUserId($userId);
+    public function getProtocolTransferById($id);
     public function deleteTransfer($uuId);
     public function createTransfer(array $transferData);
-    public function updateTransfer($uuId, array $transferData);
+    public function updateTransferByProtocol($protocol, array $transferData);
 }
